21d6e5e8e0a015283eaf3e6b7481d5d691b6f031
gkumar-pandey <gautamshekhar078@gmail.com>
2022-08-19 14:55:49 +0530

    add footer in home page

--- src/Components/Footer/SocialMediaIconsLink.js
+++ src/Components/Footer/SocialMediaIconsLink.js
@@ -1,45 +1,22 @@
 import React from "react";
-import fbIcon from "../../Assets/SocialMediaIcons/facebook.png";
-import instaIcon from "../../Assets/SocialMediaIcons/instagram.png";
-import linkedinIcon from "../../Assets/SocialMediaIcons/linkedin.png";
-import twitterIcon from "../../Assets/SocialMediaIcons/twitter.png";
-import youtube from "../../Assets/SocialMediaIcons/youtube.png";
-
-const socialMedia = [
-  {
-    icon: fbIcon,
-    link: "https://www.facebook.com/ncechirag/",
-  },
-  {
-    icon: instaIcon,
-    link: "",
-  },
-  {
-    icon: linkedinIcon,
-    link: "https://www.linkedin.com/company/chirag-an-initiative-of-nceians/about/?viewAsMember=true",
-  },
-  {
-    icon: twitterIcon,
-    link: "https://twitter.com/ncechirag",
-  },
-  {
-    icon: youtube,
-    link: "https://www.youtube.com/channel/UCaIHyPCg1q-DwZ46AM48H1w",
-  },
-];
+import socialMedia from "../../Data/Data";
 
 function SocialMediaIconsLink() {
   return (
     <>
       <div className="social-media-contianer">
-        <p>We are on Social Media :</p>
+        <p>We are on Social Media :)</p>
         {socialMedia.map((item, idx) => {
           return (
-            <a href={item.link} target='_blank_' >
+            <a href={item.link} target="_blank_">
               <img className="social-media-icon" src={item.icon} />
             </a>
           );
         })}
+      </div>
+      <div className="copyright-container">
+        <p>Copyright &#169; 2022 Chirag All Rights Reserved</p>
+        <p>Developed and Maintained By:~ <a href="">Gautam..</a></p>
       </div>
     </>
   );
---
+++ src/Data/Data.js
@@ -0,0 +1,29 @@
+import fbIcon from "../Assets/SocialMediaIcons/facebook.png";
+import instaIcon from "../Assets/SocialMediaIcons/instagram.png";
+import linkedinIcon from "../Assets/SocialMediaIcons/linkedin.png";
+import twitterIcon from "../Assets/SocialMediaIcons/twitter.png";
+import youtube from "../Assets/SocialMediaIcons/youtube.png";
+const socialMedia = [
+  {
+    icon: fbIcon,
+    link: "https://www.facebook.com/ncechirag/",
+  },
+  {
+    icon: instaIcon,
+    link: "",
+  },
+  {
+    icon: linkedinIcon,
+    link: "https://www.linkedin.com/company/chirag-an-initiative-of-nceians/about/?viewAsMember=true",
+  },
+  {
+    icon: twitterIcon,
+    link: "https://twitter.com/ncechirag",
+  },
+  {
+    icon: youtube,
+    link: "https://www.youtube.com/channel/UCaIHyPCg1q-DwZ46AM48H1w",
+  },
+];
+
+export default socialMedia;
