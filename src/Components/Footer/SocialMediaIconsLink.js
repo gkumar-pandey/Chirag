25f0460190e5d32f93f60e4a709fc6f11c82ffa6
gkumar-pandey <gautamshekhar078@gmail.com>
2022-08-20 23:42:34 +0530

    update footer

--- src/Components/Footer/SocialMediaIconsLink.js
+++ src/Components/Footer/SocialMediaIconsLink.js
@@ -3,7 +3,7 @@ import socialMedia from "../../Data/Data";
 
 function SocialMediaIconsLink() {
   return (
-    <div className="footer" >
+    <div className="footer">
       <div className="social-media-contianer">
         <p>We are on Social Media :)</p>
         {socialMedia.map((item, idx) => {
@@ -16,7 +16,19 @@ function SocialMediaIconsLink() {
       </div>
       <div className="copyright-container">
         <p>Copyright &#169; 2022 Chirag All Rights Reserved</p>
-        <p>Developed By:~ <a href="" style={{color:'#fff',fontSize:'1.2rem',textDecoration:'none'}} >Vanisha</a></p>
+        <p>
+          Developed By:~{" "}
+          <a
+            href="https://gautamkp.netlify.app/"
+            style={{
+              color: "#fff",
+              fontSize: "1.2rem",
+              textDecoration: "none",
+            }}
+          >
+            Gautam..
+          </a>
+        </p>
       </div>
     </div>
   );
